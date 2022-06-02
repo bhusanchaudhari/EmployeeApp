070a1db68c5a08d7ec60a90e648d064e288bcc6a
bhushan020 <96042738+bhushan020@users.noreply.github.com>
2022-06-02 16:49:44 +0530

    Today's work

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello World")
+//Importing express Library to create server
+const express = require("express")
+
+
+const app= express()
+
+app.get("/user",(req,res)=>{
+
+    res.send("Frist API Call")
+})
+const port=5000
+
+app.listen(port,(req,res)=>{
+    console.log(`Starting Application on port : http://localhost:${port}`)
+})
+
